41da169d61a9c57cd2bc65b243f00204a7cc754b
Debasree-Paul <87644837+Debasree-Paul@users.noreply.github.com>
2021-07-19 15:37:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const cors = require("cors");
+const exp = require("express");
+const bp = require("body-parser");
+const passport = require("passport");
+const { connect } = require("mongoose");
+const { success, error } = require("consola");
+
+
+
+const { DB, PORT } = require("./config");
+
+
+const app = exp();
+
+
+app.use(cors());
+app.use(bp.json());
+app.use(passport.initialize());
+
+require("./middlewares/passport")(passport);
+
+app.use("/api/users", require("./routes/users"));
+
+const startApp = async () => {
+  try {
+
+    await connect(DB, {
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+
+    success({
+      message: `Successfully connected with the Database \n${DB}`,
+      badge: true
+    });
+
+
+    app.listen(PORT, () =>
+      success({ message: `Server started on PORT ${PORT}`, badge: true })
+    );
+  } catch (err) {
+    error({
+      message: `Unable to connect with Database \n${err}`,
+      badge: true
+    });
+    startApp();
+  }
+};
+
+startApp();
